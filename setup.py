dd0961d272bcf6dfff53dcda97f06c593487bcf8
Guillaume St-Onge <stongeg1@gmail.com>
2022-10-16 13:00:45 -0400

    update setup, now install works on Mac

--- setup.py
+++ setup.py
@@ -37,109 +37,34 @@
 # other computer software, distribute, and sublicense such enhancements or
 # derivative works thereof, in binary and source code form.
 
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
-import sys
+
+from glob import glob
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup
 import setuptools
 
 __version__ = '0.0.1'
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
-
 ext_modules = [
-    Extension(
-        '_schon',
-        ['src/bind_schon.cpp','src/BipartiteNetwork.cpp',
-        'src/BaseContagion.cpp','src/GroupSIS.cpp', 'src/DiscreteSIS.cpp',
-        'src/HeterogeneousExposure.cpp', 'src/ContinuousSIS.cpp',
-        'src/MarginalInfectionProbability.cpp', 'src/Prevalence.cpp',
-        'src/SamplableSet/BinaryTree.cpp','src/SamplableSet/SamplableSet.cpp',
-        'src/SamplableSet/HashPropensity.cpp'],
-        include_dirs=['src/',
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True)
-        ],
-        language='c++'
-    ),
+    Pybind11Extension("_pgfgleamcpp",
+        sorted(glob("src/[!_]*.cpp")),  # Sort source files for reproducibility
+        include_dirs = ["src/"],
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
 ]
-
-
-# As of Python 3.6, CCompiler has a `has_flag` method.
-# cf http://bugs.python.org/issue26689
-def has_flag(compiler, flagname):
-    """Return a boolean indicating whether a flag name is supported on
-    the specified compiler.
-    """
-    import tempfile
-    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
-        f.write('int main (int argc, char **argv) { return 0; }')
-        try:
-            compiler.compile([f.name], extra_postargs=[flagname])
-        except setuptools.distutils.errors.CompileError:
-            return False
-    return True
-
-def cpp_flag(compiler):
-    """Return the -std=c++17 compiler flag.
-
-    c++17 is required.
-    """
-    if has_flag(compiler, '-std=c++17'):
-        return '-std=c++17'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++17 support '
-                           'is needed!')
-
-class BuildExt(build_ext):
-    """A custom build extension for adding compiler-specific options."""
-    c_opts = {
-        'msvc': ['/EHsc'],
-        'unix': [],
-    }
-
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
-
-    def build_extensions(self):
-        ct = self.compiler.compiler_type
-        opts = self.c_opts.get(ct, [])
-        if ct == 'unix':
-            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
-            opts.append(cpp_flag(self.compiler))
-            if has_flag(self.compiler, '-fvisibility=hidden'):
-                opts.append('-fvisibility=hidden')
-        elif ct == 'msvc':
-            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
-        for ext in self.extensions:
-            ext.extra_compile_args = opts
-        build_ext.build_extensions(self)
 
 
 setup(
     name='schon',
     version=__version__,
     author='Guillaume St-Onge',
-    author_email='guillaume.st-onge.4@ulaval.ca',
+    author_email='stongeg1@gmail.com',
     url='https://github.com/gstonge/schon',
     description='Simulation of Contagion on Higher-Order Networks',
     long_description='',
     packages=setuptools.find_packages(),
     ext_modules=ext_modules,
     install_requires=['pybind11>=2.2'],
-    cmdclass={'build_ext': BuildExt},
+    cmdclass={'build_ext': build_ext},
     zip_safe=False,
 )
